4ee1157f3c69c8d516acef795e14cb54e4e6122d
vyadav01 <v2013y@gmail.com>
2023-10-31 23:07:39 +0530

    api url changed

--- script.js
+++ script.js
@@ -59,210 +59,182 @@ seriesNameText.setAttribute("class", "seriesNameText");
 liveScoreText.textContent = "Live Cricket Scores";
 seriesNameText.textContent = "ICC CRICKET WORLD CUP INDIA 2023";
 
-const url =
-  "https://api.cricapi.com/v1/currentMatches?apikey=398e42ac-b41d-4dc6-80e0-07b64160da58&offset=0";
-
-const series_id = "bd830e89-3420-4df5-854d-82cfab3e1e04";
-
+const url = "https://api.cricapi.com/v1/cricScore?apikey=398e42ac-b41d-4dc6-80e0-07b64160da58";
+const firstMatchDateTimeStr = "05:00:00";
+const secondMatchDateTimeStr = "08:30:00";
 const countryFlag = {
-  India: "IN",
-  Australia: "AU",
-  Pakistan: "PK",
-  Sri_Lanka: "LK",
-  Afghanistan: "AF",
-  New_Zealand: "NZ",
-  South_Africa: "ZA",
-  England: "EN",
-  Bangladesh: "BD",
-  Netherlands: "NL",
+    India: "IN",
+    Australia: "AU",
+    Pakistan: "PK",
+    Sri_Lanka: "LK",
+    Afghanistan: "AF",
+    New_Zealand: "NZ",
+    South_Africa: "ZA",
+    England: "EN",
+    Bangladesh: "BD",
+    Netherlands: "NL",
 };
-
-// fetching the api data and show it on page
+const countryShortNameArray = [];
+const teamsNameArray = [];
+const teamNameStrArray = [];
+const teamShortNameArray = [];
 fetch(url)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-    const apiData = data.data;
-    
-    const dataLength = apiData.length;
-    
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+        const apiData = data.data;
+        const dataLength = apiData.length;
 
-    if (data.status === "success") {
-      for (let index = 0; index < dataLength; index++) {
-        if (series_id === apiData[index].series_id) {
-          const liveMatchData = apiData[index];
-          const matchCountryFlag = [];
 
-          for (let index = 0; index < liveMatchData.teamInfo.length; index++) {
-            const element = liveMatchData.teamInfo[index].name;
+        if (data.status === "success") {
+            for (let index = 0; index < dataLength; index++) {
+                const dateTimeString = new Date(`${apiData[index].dateTimeGMT}`);
 
-            const newElement = element.replace(" ", "_");
+                if (
+                    firstMatchDateTimeStr === dateTimeString.toLocaleTimeString() ||
+                    secondMatchDateTimeStr === dateTimeString.toLocaleTimeString()
+                ) {
 
-            for (const key in countryFlag) {
-              if (newElement === key) {
-                matchCountryFlag.push(`${countryFlag[key]}`);
-              }
+                    // console.log(apiData[index]);
+                    teamsNameArray.splice(0, teamsNameArray.length);
+                    teamsNameArray.push(apiData[index].t1);
+                    teamsNameArray.push(apiData[index].t2);
+                    countryShortNameArray.splice(0, countryShortNameArray.length);
+
+                    for (const iterator of teamsNameArray) {
+                        let newtext = iterator.replace(" ", "_");
+                        // console.log(newtext);
+                        for (const key in countryFlag) {
+                            if (newtext.includes(key)) {
+                                countryShortNameArray.push(countryFlag[key])
+                            };
+
+
+                        }
+                    }
+                    
+
+                    const setTime = new Date();
+                    setTime.setTime(dateTimeString.getTime() + 19800000);
+                    const timeFormat = new Intl.DateTimeFormat("en-us", {
+                        timeStyle: "short",
+                    });
+
+                    // creating html elements
+                    const matchWrapperDiv = document.createElement("div");
+                    const divMatchInfo = document.createElement("div");
+                    const matchName = document.createElement("h3");
+                    const venue = document.createElement("h4");
+                    const divTeamsNameFlag = document.createElement("div");
+                    const divTeamFirst = document.createElement("div");
+                    const imgFirstTeamFlag = document.createElement("img");
+                    const firstTeamNameText = document.createElement("h2");
+                    const divVs = document.createElement("div");
+                    const vsText = document.createElement("p");
+                    const divTeamSecond = document.createElement("div");
+                    const imgSecondTeamFlag = document.createElement("img");
+                    const secondTeamNameText = document.createElement("h2");
+                    const divScoreInfo = document.createElement("div");
+                    const firstTeamRunsWickets = document.createElement("p");
+                    const secondTeamRunsWickets = document.createElement("p");
+                    const matchStatusText = document.createElement("p");
+
+                    // attributs
+                    matchWrapperDiv.setAttribute("class", "matchWrapperDiv");
+                    divMatchInfo.setAttribute("class", "matchInfo");
+                    matchName.setAttribute("class", "matchName");
+                    venue.setAttribute("class", "venue");
+                    divTeamsNameFlag.setAttribute("class", "teamsNameFlag");
+                    divTeamFirst.setAttribute("class", "team");
+                    imgFirstTeamFlag.setAttribute("class", "teamflag");
+                    imgFirstTeamFlag.setAttribute("alt", "team1_flag");
+                    firstTeamNameText.setAttribute("class", "teamName");
+                    divVs.setAttribute("class", "vs");
+                    divTeamSecond.setAttribute("class", "team");
+                    imgSecondTeamFlag.setAttribute("class", "teamflag");
+                    imgSecondTeamFlag.setAttribute("alt", "team2_flag");
+                    secondTeamNameText.setAttribute("class", "teamName");
+                    divScoreInfo.setAttribute("class", "scoreInfo");
+                    firstTeamRunsWickets.setAttribute("class", "runsWickets");
+                    secondTeamRunsWickets.setAttribute("class", "runsWickets");
+                    matchStatusText.setAttribute("class", "matchStatus");
+
+                    imgFirstTeamFlag.setAttribute("src", `https://cdorg.b-cdn.net/flags/generic/${countryShortNameArray[0]}.svg`);
+
+                    imgSecondTeamFlag.setAttribute("src", `https://cdorg.b-cdn.net/flags/generic/${countryShortNameArray[1]}.svg`);
+
+                    
+                    teamNameStrArray.splice(0, teamNameStrArray.length);
+                    for (const iterator of teamsNameArray) {
+                        const teamNameStr = iterator.slice(0, iterator.length - 5).trim();
+                        teamNameStrArray.push(teamNameStr);
+                        
+                    }
+                    
+                    teamShortNameArray.splice(0, teamShortNameArray.length);
+                    for (const iterator of teamsNameArray) {
+                        const teamShortNameStr = iterator.slice(-5).trim(); 
+                        const tShorthandName = teamShortNameStr.slice(1, teamShortNameStr.length - 1);
+                        teamShortNameArray.push(tShorthandName);
+                        
+                    }
+                    matchName.textContent =
+                        `${teamNameStrArray[0]}  v/s  ${teamNameStrArray[1]}, ${apiData[index].matchType}, ${apiData[index].ms}`.toUpperCase();
+                    venue.textContent = `${dateTimeString.toDateString()}, ${timeFormat.format(
+                        setTime
+                    )}`;
+                    firstTeamNameText.textContent = `${teamShortNameArray[0]}`;
+                    vsText.textContent = "v/s";
+                    secondTeamNameText.textContent = `${teamShortNameArray[1]}`;
+
+                    firstTeamRunsWickets.textContent = `${apiData[index].t1s}`;
+
+                    secondTeamRunsWickets.textContent = `${apiData[index].t2s}`;
+
+                    matchStatusText.textContent = `${apiData[index].status}`;
+
+                    // appending html elements in the body tag of second section
+                    secondSection.appendChild(matchWrapperDiv);
+                    matchWrapperDiv.appendChild(divMatchInfo);
+                    divMatchInfo.appendChild(matchName);
+                    divMatchInfo.appendChild(venue);
+                    matchWrapperDiv.appendChild(divTeamsNameFlag);
+                    divTeamsNameFlag.appendChild(divTeamFirst);
+                    divTeamFirst.appendChild(imgFirstTeamFlag);
+                    divTeamFirst.appendChild(firstTeamNameText);
+                    divTeamFirst.appendChild(firstTeamRunsWickets);
+                    divTeamsNameFlag.appendChild(divVs);
+                    divVs.appendChild(vsText);
+                    divTeamsNameFlag.appendChild(divTeamSecond);
+                    divTeamSecond.appendChild(imgSecondTeamFlag);
+                    divTeamSecond.appendChild(secondTeamNameText);
+                    divTeamSecond.appendChild(secondTeamRunsWickets);
+                    matchWrapperDiv.appendChild(divScoreInfo);
+                    divScoreInfo.appendChild(matchStatusText);
+
+                }
+
             }
-          }
-
-          const dateTime = new Date(`${apiData[index].dateTimeGMT}`);
-          const setTime = new Date();
-          setTime.setTime(dateTime.getTime() + 19800000);
-          const timeFormat = new Intl.DateTimeFormat("en-us", {
-            timeStyle: "short",
-          });
-
-          // creating html elements
-          const matchWrapperDiv = document.createElement("div");
-          const divMatchInfo = document.createElement("div");
-          const matchName = document.createElement("h3");
-          const venue = document.createElement("h4");
-          const divTeamsNameFlag = document.createElement("div");
-          const divTeamFirst = document.createElement("div");
-          const imgFirstTeamFlag = document.createElement("img");
-          const firstTeamNameText = document.createElement("h2");
-          const divVs = document.createElement("div");
-          const vsText = document.createElement("p");
-          const divTeamSecond = document.createElement("div");
-          const imgSecondTeamFlag = document.createElement("img");
-          const secondTeamNameText = document.createElement("h2");
-          const divScoreInfo = document.createElement("div");
-          const divMatchScore = document.createElement("div");
-          const divTeamScoreFirst = document.createElement("div");
-          const firstTeamInningText = document.createElement("p");
-          const firstTeamRunsWickets = document.createElement("p");
-          const firstLineBreaker = document.createElement("br");
-          const firstTeamOvers = document.createElement("span");
-          const divTeamScoreSecond = document.createElement("div");
-          const secondTeamInningText = document.createElement("p");
-          const secondTeamRunsWickets = document.createElement("p");
-          const secondLineBreaker = document.createElement("br");
-          const secondTeamOvers = document.createElement("span");
-          const matchStatusText = document.createElement("p");
-
-          // attributs
-          matchWrapperDiv.setAttribute("class", "matchWrapperDiv");
-          divMatchInfo.setAttribute("class", "matchInfo");
-          matchName.setAttribute("class", "matchName");
-          venue.setAttribute("class", "venue");
-          divTeamsNameFlag.setAttribute("class", "teamsNameFlag");
-          divTeamFirst.setAttribute("class", "team");
-          imgFirstTeamFlag.setAttribute("class", "teamflag");
-          imgFirstTeamFlag.setAttribute("alt", "team1_flag");
-          firstTeamNameText.setAttribute("class", "teamName");
-          divVs.setAttribute("class", "vs");
-          divTeamSecond.setAttribute("class", "team");
-          imgSecondTeamFlag.setAttribute("class", "teamflag");
-          imgSecondTeamFlag.setAttribute("alt", "team2_flag");
-          secondTeamNameText.setAttribute("class", "teamName");
-          divScoreInfo.setAttribute("class", "scoreInfo");
-          divMatchScore.setAttribute("class", "matchScore");
-          divTeamScoreFirst.setAttribute("class", "teamScore");
-          firstTeamInningText.setAttribute("class", "inningText");
-          firstTeamRunsWickets.setAttribute("class", "runsWickets");
-          firstTeamOvers.setAttribute("class", "overs");
-          divTeamScoreSecond.setAttribute("class", "teamScore");
-          secondTeamInningText.setAttribute("class", "inningText");
-          secondTeamRunsWickets.setAttribute("class", "runsWickets");
-          secondTeamOvers.setAttribute("class", "overs");
-          matchStatusText.setAttribute("class", "matchStatus");
-
-          imgFirstTeamFlag.setAttribute(
-            "src",
-            `https://cdorg.b-cdn.net/flags/generic/${matchCountryFlag[0]}.svg`
-          );
-
-          imgSecondTeamFlag.setAttribute(
-            "src",
-            `https://cdorg.b-cdn.net/flags/generic/${matchCountryFlag[1]}.svg`
-          );
-
-          matchName.textContent =
-            `${apiData[index].name}, ${apiData[index].matchType}`.toUpperCase();
-          venue.textContent = `${dateTime.toDateString()}, ${timeFormat.format(
-            setTime
-          )}, ${apiData[index].venue}`;
-          firstTeamNameText.textContent = `${apiData[index].teamInfo[0].shortname}`;
-          vsText.textContent = "v/s";
-          secondTeamNameText.textContent = `${apiData[index].teamInfo[1].shortname}`;
-          let x;
-          let str = ""
-          if (apiData[index].score.length === 2) {
-            x = apiData[index].score[0].inning;
-            str += x;
-
-            if (str.includes(`${apiData[index].teamInfo[0].name}`)) {
-              firstTeamInningText.textContent = `${apiData[index].score[0].inning}`;
-              firstTeamRunsWickets.textContent = `${apiData[index].score[0].r}-${apiData[index].score[0].w} (${apiData[index].score[0].o})`;
-              secondTeamInningText.textContent = `${apiData[index].score[1].inning}`;
-              secondTeamRunsWickets.textContent = `${apiData[index].score[1].r}-${apiData[index].score[1].w} (${apiData[index].score[1].o})`;
-            } else {
-              firstTeamInningText.textContent = `${apiData[index].score[1].inning}`;
-              firstTeamRunsWickets.textContent = `${apiData[index].score[1].r}-${apiData[index].score[1].w} (${apiData[index].score[1].o})`;
-              secondTeamInningText.textContent = `${apiData[index].score[0].inning}`;
-              secondTeamRunsWickets.textContent = `${apiData[index].score[0].r}-${apiData[index].score[0].w} (${apiData[index].score[0].o})`;
-            }
-          } else if (apiData[index].score.length === 1) {
-            x = apiData[index].score[0].inning;
-            str += x;
-            if (str.includes(`${apiData[index].teamInfo[0].name}`)) {
-              firstTeamInningText.textContent = `${apiData[index].score[0].inning}`;
-              firstTeamRunsWickets.textContent = `${apiData[index].score[0].r}-${apiData[index].score[0].w}  (${apiData[index].score[0].o})`;
-              secondTeamInningText.textContent = "Bowling side";
-              secondTeamRunsWickets.textContent = "Yet to Bat";
-            } else {
-              firstTeamInningText.textContent = "Bowling side";
-              firstTeamRunsWickets.textContent = "Yet to Bat";
-              secondTeamInningText.textContent = `${apiData[index].score[0].inning}`;
-              secondTeamRunsWickets.textContent = `${apiData[index].score[0].r}-${apiData[index].score[0].w}  (${apiData[index].score[0].o})`;
-            }
-          } else {
-            divMatchScore.style.display = "none";
-          }
-          matchStatusText.textContent = `${apiData[index].status}`;
-
-          // appending html elements in the body tag of second section
-          secondSection.appendChild(matchWrapperDiv);
-          matchWrapperDiv.appendChild(divMatchInfo);
-          divMatchInfo.appendChild(matchName);
-          divMatchInfo.appendChild(venue);
-          matchWrapperDiv.appendChild(divTeamsNameFlag);
-          divTeamsNameFlag.appendChild(divTeamFirst);
-          divTeamFirst.appendChild(imgFirstTeamFlag);
-          divTeamFirst.appendChild(firstTeamNameText);
-          divTeamsNameFlag.appendChild(divVs);
-          divVs.appendChild(vsText);
-          divTeamsNameFlag.appendChild(divTeamSecond);
-          divTeamSecond.appendChild(imgSecondTeamFlag);
-          divTeamSecond.appendChild(secondTeamNameText);
-          matchWrapperDiv.appendChild(divScoreInfo);
-          divScoreInfo.appendChild(divMatchScore);
-          divMatchScore.appendChild(divTeamScoreFirst);
-          divTeamScoreFirst.appendChild(firstTeamInningText);
-          divTeamScoreFirst.appendChild(firstTeamRunsWickets);
-          firstTeamRunsWickets.appendChild(firstLineBreaker);
-          firstTeamRunsWickets.appendChild(firstTeamOvers);
-          divMatchScore.appendChild(divTeamScoreSecond);
-          divTeamScoreSecond.appendChild(secondTeamInningText);
-          divTeamScoreSecond.appendChild(secondTeamRunsWickets);
-          secondTeamRunsWickets.appendChild(secondTeamOvers);
-          secondTeamRunsWickets.appendChild(secondLineBreaker);
-          divScoreInfo.appendChild(matchStatusText);
+        } else {
+            // const errorMessageDiv = document.createElement("div");
+            // const errorText = document.createElement("p");
+            // errorMessageDiv.setAttribute("id", "errorMessageDiv");
+            // errorText.setAttribute("id", "errorText");
+            // errorText.textContent = "Today's api hits limit exceeded.";
+            // secondSection.appendChild(errorMessageDiv);
+            // errorMessageDiv.appendChild(errorText);
+            error("Today's api hits limit exceeded.");
         }
-      }
-    } else {
-      const errorMessageDiv = document.createElement("div");
-      const errorText = document.createElement("p");
-      errorMessageDiv.setAttribute("id", "errorMessageDiv");
-      errorText.setAttribute("id", "errorText");
-      errorText.textContent = "Today's api hits limit exceeded.";
-      secondSection.appendChild(errorMessageDiv);
-      errorMessageDiv.appendChild(errorText);
-    }
-  })
-  .catch((error) => console.log(`Error: ${error}`));
+        // console.log(teamsNameArray);
+        // console.log(countryShortNameArray);
+    })
+.catch((err) => {
+    // console.log(`Error: ${error}`)
+    error(err);
+    
+});
 
 // appending html elements in the body tag of second section
 document.body.appendChild(secondSection);
@@ -270,3 +242,13 @@ secondSection.appendChild(divUpperHeading);
 divUpperHeading.appendChild(liveScoreText);
 secondSection.appendChild(divSeriesName);
 divSeriesName.appendChild(seriesNameText);
+
+function error(errorMessage) {
+    const errorMessageDiv = document.createElement("div");
+    const errorText = document.createElement("p");
+    errorMessageDiv.setAttribute("id", "errorMessageDiv");
+    errorText.setAttribute("id", "errorText");
+    errorText.textContent = errorMessage;
+    secondSection.appendChild(errorMessageDiv);
+    errorMessageDiv.appendChild(errorText);
+}
